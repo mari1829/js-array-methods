d66c7b84bcca42987bfaf372ac186bc6a73cf5bd
mari <sermarajraj35@gmail.com>
2022-08-08 08:57:16 -0800

    array-methods

---
+++ splice-method/script.js
@@ -0,0 +1,7 @@
+let a=[12,23,45,78,90]
+console.log(a.splice(0,2));
+console.log(a);
+
+let b=["mari","leo","selvam","arun","partha"]
+console.log(b.splice(1,3));
+console.log(b);
---
+++ tostring-method/script.js
@@ -0,0 +1,5 @@
+let a=["mari","ram","bala","vasanth"]
+console.log(a.toString());
+
+let b=["1","3","5","7"]
+console.log(b.toString());
