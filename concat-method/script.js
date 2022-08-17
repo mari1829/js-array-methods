fc8e38aea61f4a4989760e7ba22403e099beb955
mari <sermarajraj35@gmail.com>
2022-08-17 03:08:48 -0800

    js

---
+++ concat-method/script.js
@@ -0,0 +1,4 @@
+let a=["mari","bala","selvam","mani"]
+let b=["arun","harish"]
+console.log(a.concat(b));
+console.log(b.concat(a));
--- tostring-method/script.js
+++ tostring-method/script.js
@@ -1,5 +1,32 @@
 let a=["mari","ram","bala","vasanth"]
 console.log(a.toString());
 
-let b=["1","3","5","7"]
+let b=[1,3,5,7]
 console.log(b.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
